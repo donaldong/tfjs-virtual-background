1b2c0e435d9b13404065155382a3846f263d1be3
Donald Dong <ddong@chanzuckerberg.com>
2021-03-09 20:49:07 -0800

    Add base class VirtualBackground

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+
+declare type ImageType = HTMLImageElement | HTMLVideoElement | HTMLCanvasElement;
+
+export enum Effect {
+  Bokeh,
+}
+
+export class VirtualBackground {
+  constructor(source: ImageType, background: ImageType | Effect | String) {
+    console.log("source", source);
+    console.log("background", background);
+  }
+}
